e4c8698c1a46b30d7bbb1715b453ea6d3c350036
Chrisssssssy <166124668+Chrisssssssy@users.noreply.github.com>
2026-09-23 10:46:18 +0200

    Initial

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,7 @@
-void main()
-{
-    
+public class Main {
+    public static void main() {
+        Adventure adventure = new Adventure();
+        UserInterface ui = new UserInterface(adventure);
+        ui.start();
+    }
 }
--- src/Room.java
+++ src/Room.java
@@ -1,2 +1,50 @@
 public class Room {
+    private String name;
+    private String description;
+    private Room north;
+    private Room east;
+    private Room south;
+    private Room west;
+
+    public Room(String name, String description) {
+        this.name = name;
+        this.description = description;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public Room getNorth() {
+        return north;
+    }
+
+    public void setNorth(Room room) {
+        this.north = room;
+    }
+
+    public Room getEast() {
+        return east;
+    }
+    public void setEast(Room room) {
+        this.east = room;
+    }
+
+    public Room getSouth() {
+        return south;
+    }
+    public void setSouth(Room room) {
+        this.south = room;
+    }
+
+    public Room getWest() {
+        return west;
+    }
+    public void setWest(Room room) {
+        this.west = room;
+    }
 }
