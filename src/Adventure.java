1f95c8ed6d63f6ba742be033afbfaad289be57b2
Chrisssssssy <166124668+Chrisssssssy@users.noreply.github.com>
2026-09-24 15:47:46 +0200

    Update adventure and userinterfaces

--- src/Adventure.java
+++ src/Adventure.java
@@ -10,44 +10,44 @@ public class Adventure {
 
         boolean isRunning = true;
 
-        while (isRunning) {
-            String kommando = UI.readCommand();
-
-            if (kommando.startsWith("go ")) {
-                kommando = kommando.substring(3).trim();
-            }   // QoL
-
-            switch (kommando) {
-                case "north", "n" -> move(currentRoom.getNorth());
-                case "east",  "e" -> move(currentRoom.getEast());
-                case "south", "s" -> move(currentRoom.getSouth());
-                case "west",  "w" -> move(currentRoom.getWest());
-                case "look"        -> UI.printRoom(currentRoom);
-                case "help" -> {
-                    IO.println("Commands:");
-                    IO.println(" go north / north / n");
-                    IO.println(" go east  / east  / e");
-                    IO.println(" go south / south / s");
-                    IO.println(" go west  / west  / w");
-                    IO.println(" look    - describe the current room");
-                    IO.println(" help    - show this list");
-                    IO.println(" exit    - quit the game");
-                }
-                case "exit" -> {
-                    IO.println("Goodbye!");
-                    isRunning = false;
-                }
-                default -> IO.println("Unknown command. Type HELP for a list of commands.");
-            }
-        }
-    }
-
-    private void move(Room next) {
-        if (next == null) {
-            UI.printCannotGo();
-        } else {
-            currentRoom = next;
-            UI.printRoom(currentRoom);
-        }
-    }
+//        while (isRunning) {
+//            String kommando = UI.readCommand();
+//
+//            if (kommando.startsWith("go ")) {
+//                kommando = kommando.substring(3).trim();
+//            }   // QoL
+//
+//            switch (kommando) {
+//                case "north", "n" -> move(currentRoom.getNorth());
+//                case "east",  "e" -> move(currentRoom.getEast());
+//                case "south", "s" -> move(currentRoom.getSouth());
+//                case "west",  "w" -> move(currentRoom.getWest());
+//                case "look"        -> UI.printRoom(currentRoom);
+//                case "help" -> {
+//                    IO.println("Commands:");
+//                    IO.println(" go north / north / n");
+//                    IO.println(" go east  / east  / e");
+//                    IO.println(" go south / south / s");
+//                    IO.println(" go west  / west  / w");
+//                    IO.println(" look    - describe the current room");
+//                    IO.println(" help    - show this list");
+//                    IO.println(" exit    - quit the game");
+//                }
+//                case "exit" -> {
+//                    IO.println("Goodbye!");
+//                    isRunning = false;
+//                }
+//                default -> IO.println("Unknown command. Type HELP for a list of commands.");
+//            }
+//        }
+//    }
+//
+//    private void move(Room next) {
+//        if (next == null) {
+//            UI.printCannotGo();
+//        } else {
+//            currentRoom = next;
+//            UI.printRoom(currentRoom);
+//        }
+//    }
 }
--- src/UserInterface.java
+++ src/UserInterface.java
@@ -3,9 +3,21 @@ public class UserInterface {
     private final UserInterface UI = new UserInterface();
 
     public String readCommand() {
-        return IO.readln().trim().toLowerCase();
-    }
+        String kommando =  IO.readln().trim().toLowerCase();
 
+        if (kommando.startsWith("go ")) {
+            kommando = kommando.substring(3).trim();
+        }
+
+        return switch (kommando) {
+            case "n" -> "north";
+            case "e" -> "east";
+            case "s" -> "south";
+            case "w" -> "west";
+            default  -> kommando;
+        };
+
+    }
 
     public void printRoom(Room room) {
         IO.println("You are in " + room.getName());
@@ -15,5 +27,27 @@ public class UserInterface {
     public void printCannotGo() {
         IO.println("You cannot go that way");
     }
+
+    public void printHelp() {
+        IO.println("Commands:");
+        IO.println(" go north / north / n");
+        IO.println(" go east  / east  / e");
+        IO.println(" go south / south / s");
+        IO.println(" go west  / west  / w");
+        IO.println(" look    - describe the current room");
+        IO.println(" help    - show this list");
+        IO.println(" exit    - quit the game");
+    }
+
+    public void printGoodbye() {
+        IO.println("Goodbye!");
+    }
+
+    public void printUnknownCommand() {
+        IO.println("Unknown command. Type HELP for a list of commands.");
+    }
+
+
+
 
 }
